30c3a5769c039488cb2b87a28e97538d4c608553
Shravan Anand K <shravan.anand007@gmail.com>
2022-06-06 19:48:19 +0530

    modified alexnet

--- models/alexnet.py
+++ models/alexnet.py
@@ -1,37 +1,40 @@
+''' Standard code for Alexnet '''
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class AlexNet(nn.Module):
-    def __init__(self, num_classes=10):
+    def __init__(self, num_classes):
         super(AlexNet, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
+            nn.Conv2d(3, 96, kernel_size=11, stride=1, padding=0), # Actual: Stride 4
             nn.BatchNorm2d(96),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 3, stride = 2))
         self.layer2 = nn.Sequential(
-            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2), # Actual: Stride 2
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 3, stride = 2))
+            nn.MaxPool2d(kernel_size = 3, stride = 1)
+            )
         self.layer3 = nn.Sequential(
-            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1), # Actual: Stride 2
             nn.BatchNorm2d(384),
             nn.ReLU())
         self.layer4 = nn.Sequential(
-            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1), # Actual: Stride 2
             nn.BatchNorm2d(384),
             nn.ReLU())
         self.layer5 = nn.Sequential(
-            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1), # Actual: Stride 2
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 3, stride = 2))
+            nn.MaxPool2d(kernel_size = 3, stride = 1) # Actual: Stride 2
+            )
         self.fc = nn.Sequential(
             nn.Dropout(0.5),
-            nn.Linear(9216, 4096),
+            nn.Linear(9216, 4096), # Consider reducing neurons -SA
             nn.ReLU())
         self.fc1 = nn.Sequential(
             nn.Dropout(0.5),
@@ -40,7 +43,7 @@ class AlexNet(nn.Module):
         self.fc2= nn.Sequential(
             nn.Linear(4096, num_classes))
         
-    def forward(self, x):
+    def forward(self, x):  # If you want to maintain stride ratios consider reducing lareys - SA
         out = self.layer1(x)
         out = self.layer2(out)
         out = self.layer3(out)
